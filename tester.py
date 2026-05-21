4849c08d51ae57f301a73befc7c2ab69e38e9e9e
Andre Falcao <aofalcao@ciencias.ulisboa.pt>
2026-05-21 12:16:08 +0100

    Fix print statements and improve formatting

--- tester.py
+++ tester.py
@@ -5,7 +5,7 @@ import bulkMI
 try:
     from numba import jit
 except ImportError:
-    print("Numba is not installed - Code is run withount JIT")
+    print("Numba is not installed - Code is run without JIT")
 
 
 D=np.array([[0,1,0],
@@ -16,27 +16,28 @@ D=np.array([[0,1,0],
             [0,1,1],
             [1,0,0],
             [0,0,0]])
-D=D.astype(np.float32)
+
+D = D.astype(np.float32) #not strictly needed
 
 
 #res1  = buklkMIbulk_MI_torch(torch.tensor(D, dtype=int))
 res  = bulkMI.pairwise_MI(D)
 np.set_printoptions(suppress=True, precision=3)
-print("Pairwise")
+print("\nPairwise")
 print(res)
 #r_s, r_ss, r_b, r_bo, r_bs    
 
 res2  = bulkMI.bulk_MI_base(D)
-print("Bulk Simple")
+print("\nBulk Simple")
 print(res2)
 
 res3 =  bulkMI.bulk_MI(D)
-print("Bulk Optimized")
+print("\nBulk Optimized")
 print(res3)
 
 Ds = csr_matrix(D)
 res4 =  bulkMI.bulk_MI(Ds)
-print("Bulk Optimized Sparse")
+print("\nBulk Optimized Sparse")
 print(res4)
 
 
